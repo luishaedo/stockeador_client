b45f05ca715910fd5ccc0f0ef6f53cbcd8d3e5ad
luishaedo <luismariahaedolanda@gmail.com>
2025-03-24 18:46:42 -0300

    env

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -41,7 +41,7 @@ const RegisterForm = () => {
       // Imprimir datos para depuración
       console.log('Enviando datos:', { name, email, password });
      
-      const response = await axios.post(import.meta.env.VITE_API_URL, {
+      const response = await axios.post(import.meta.env.VITE_API_URL+"/api/auth/register", {
         name,
         email,
         password
